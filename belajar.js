f174d85ac7c41f686db6433d5bedc3c6bcfc5b9a
MAIKEL DELL <figther659@gmail.com>
2025-10-20 21:36:50 -0700

    Convert belajar.js to a complete HTML document

--- belajar.js
+++ belajar.js
@@ -1,21 +1,46 @@
-<template>
-    
-</template>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Animasi Sederhana</title>
+  <style>
+    body {
+      background-color: #f0f0f0;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
 
-<scrip setup>
+    #box {
+      width: 100px;
+      height: 100px;
+      background-color: tomato;
+      position: relative;
+      border-radius: 10px;
+    }
+  </style>
+</head>
+<body>
 
+  <div id="box"></div>
 
+  <script>
+    const box = document.getElementById('box');
+    let posisi = 0;
 
+    function gerak() {
+      if (posisi < 300) { 
+        posisi++;
+        box.style.left = posisi + 'px';
+        requestAnimationFrame(gerak); 
+      }
+    }
 
+    gerak(); 
+  </script>
 
+</body>
+</html>
 
-
-
-
-
-
-<scrip> 
-
-    <style lang="scss" scoped>
-
-        </style>
