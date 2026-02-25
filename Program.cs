5b0e4280fedd006f2e218d119f2344ef8dc49b26
MAIKEL DELL <figther659@gmail.com>
2026-02-25 21:52:30 +0900

    tugas
    Signed-off-by: MAIKEL DELL <figther659@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] penerima = {"armando", "alfa", "simson", "dahlia", "maikel",};
+int [] beasiswa = {1000000, 2000000, 3000000, 4000000, 5000000};
+
+for (int i = 0; i < penerima.Length; i++)
+{
+    Console.WriteLine($" daftar penerima beasiswa: {penerima[i]}, jumlah beasiswa: {beasiswa[i]}");
+};
+
+Console.WriteLine("penerima[i] = " + penerima[1] + " beasiswa[i] = " + beasiswa[1]);
